8cfe31390d8a7ea467784f0830c5bc7900957045
Rosendoxx <leandrosoft0813@gmail.com>
2026-01-27 18:18:10 -0300

    Categoria controler inicial

--- controller/CategoriaController.java
+++ controller/CategoriaController.java
@@ -1,4 +1,46 @@
 package controller;
 
+import dao.CategoriaDAO;
+import model.Categoria;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class CategoriaController {
+    private CategoriaDAO cdao;
+
+    public CategoriaController(){
+        this.cdao = new CategoriaDAO();
+    }
+
+    public List<Categoria> listarTodos(){
+        return new ArrayList<>(cdao.buscarTodos());
+    }
+
+    public void cadastrar(int id, String nome){
+        if(!Objects.isNull(cdao.buscarPorId(id))){
+            System.out.println("Essa categoria já existe\nNão é possível cadastrar");
+            return;
+        }
+        if(cdao.buscarTodos().stream().filter(c -> c.getNome().equalsIgnoreCase(nome)).isParallel()){
+            System.out.println("Já existe uma categoria com esse nome\nNão é possível cadastrar");
+            return;
+        }
+        try{
+            cdao.inserir(new Categoria(id, nome)); //Observar abstração do ENUM
+            System.out.println("Categoria adcionada com sucesso");
+        } catch (RuntimeException e){
+            System.out.println("Erro ao inserir categoria "+ e);
+        }
+    }
+
+    public void excluir(int id){
+        try{
+            cdao.excluir(id);
+            System.out.println("Categoria excluida com sucesso");
+        } catch (RuntimeException e){
+            System.out.println("Erro ao excluir categoria "+e);
+        }
+    }
 }
--- model/Categoria.java
+++ model/Categoria.java
@@ -3,23 +3,53 @@ package model;
 import java.util.Comparator;
 
 public enum Categoria implements Comparator<Categoria>{
-	SEM_CATEGORIA, ALIMENTICIO, ELETRONICO, LIMPEZA, VESTUARIO;
-	
+	SEM_CATEGORIA(0, "Sem Categoria"), ALIMENTICIO(1, "Alimentício"), ELETRONICO(2, "Eletrônico"), LIMPEZA(3, "Limpeza"), VESTUARIO(4, "Vestuário");
+
+	private int id;
+	private String nome;
+
+	Categoria(int id, String nome){
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public static void mostrarCategorias() {
 		System.out.println("1 - Alimentício |  2 - Eletrônico | 3 - Limpeza | 4 - Vestuário");
 	}
-	
+
 	public static Categoria getPorId(int idCategoria) {
 		for(Categoria c: values()) {
-			if(c.ordinal() == idCategoria) {
+			if(c.getId() == idCategoria) {
 				return c;
 			}
 		}
 		return null; //retorna null se não encontrar categoria válida
 	}
+
+	@Override
+	public String toString(){
+		return getNome();
+	}
+
 	@Override
 	public int compare(Categoria o1, Categoria o2) {
 		return o1.compareTo(o2);
 	}
-	
+
 }
